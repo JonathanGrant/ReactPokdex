0868fd2b5828bd984e1061220edb067c011e9e46
Jonathan Grant <jgrant114@bloomberg.net>
2018-12-24 18:14:40 -0500

    Added gpages settings

--- src/App.js
+++ src/App.js
@@ -100,7 +100,7 @@ class App extends Component {
 
   render() {
     return (
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <Route path='/:id?' component={MyPokedex}/>
       </Router>
     );
